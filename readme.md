b9452417cf842e2901bc7a0d22081b7f95a52b27
Isaiah <91203559+ilindley22@users.noreply.github.com>
2024-12-30 13:29:25 -0600

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,18 @@
+**Batch Image File Converter**
+
+This program converts all image files in the desired directory
+from one format to another
+
+The default conversion is .webp to .png
+The default search folder is the users' /Downloads folder
+
+**HOW TO USE/INSTALL**
+
+1. (Optional) Change default path to desired path
+2. (Optional) Change default file converstion
+3. Run script
+4. Proft!
+
+**Enjoy! :)**
+
+_Last updated ~ 12/30/2024_
---
+++ webpconverter.py
@@ -0,0 +1,28 @@
+import os
+import glob
+from PIL import Image
+from pathlib import Path
+
+fileCount = 0
+
+# Gets users' downloads folder path
+path = Path.home() / "Downloads" #DEFAULT PATH
+
+# Gathers all files
+image_list = path.glob("*.webp") 
+
+for file in image_list:
+
+    fileName, ext = os.path.splitext(file)
+    outFile = fileName + ".jpg" #DEFAULT CONVERSION
+    image = Image.open(file)
+    image.save(outFile)
+    os.remove(file)  # Deletes old file
+    fileCount += 1
+
+# Print messages
+if fileCount > 0:
+    print("Script Complete!")
+    print("# of files changed:", fileCount)
+else:
+    print("No files to convert")
